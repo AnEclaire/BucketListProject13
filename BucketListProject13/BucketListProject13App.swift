e88fa4ba5b9389c43127a8ecc0d0560b584acb18
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-19 23:18:34 -0700

    Initial Commit

---
+++ BucketListProject13/BucketListProject13App.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListProject13App.swift
+//  BucketListProject13
+//
+//  Created by Emma Gutierrez on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListProject13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
